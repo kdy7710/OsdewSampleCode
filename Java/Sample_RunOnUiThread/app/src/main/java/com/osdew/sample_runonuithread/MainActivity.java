02b0d29db420b15aa8f7324fd3e6ca84e03a9fd2
KimDaeYoung <kdy77110@gmail.com>
2021-03-28 16:08:18 +0900

    Sample_RunOnUiThread

---
+++ Java/Sample_RunOnUiThread/app/src/main/java/com/osdew/sample_runonuithread/MainActivity.java
@@ -0,0 +1,14 @@
+package com.osdew.sample_runonuithread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
